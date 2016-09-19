dd5dfdaa44aaa09c41f32787111ecac55de7c38f
Rick Kasten <rickkasten@gmail.com>
2016-09-18 20:57:08 -0400

    Added cleanup, better local version check

--- brew-cask-upgrade.sh
+++ brew-cask-upgrade.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo Executing \'brew cleanup\; brew cask cleanup\; brew update\'
+brew cleanup; brew cask cleanup; brew update
 for pkg in $(brew cask list); do
     pkg_latest=$(brew cask info $pkg | grep $pkg: | awk '{print $2}')
     [ "$pkg_latest" == "latest" ] && continue
-    pkg_current=$(brew cask cat $pkg | grep '  version ' | awk '{print $2}')
-    if [[ "$pkg_latest" != "$pkg_current" && "'$pkg_latest'" != "$pkg_current" ]]; then
+    if [ ! -d /usr/local/Caskroom/$pkg/$pkg_latest ]; then
         echo $pkg
         brew cask fetch $pkg
         brew cask install $pkg --force
