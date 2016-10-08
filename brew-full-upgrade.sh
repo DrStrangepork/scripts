6a1cd36839c5326fd1ecf38536b613147de1be31
Rick Kasten <rickkasten@gmail.com>
2016-10-08 12:09:22 -0400

    Moved casks to their own loop, added colors to match brew-cask provides a friendly homebrew-style CLI workflow for the administration of macOS applications distributed as binaries.
    Commands:
    
        audit                  verifies installability of Casks
        cat                    dump raw source of the given Cask to the standard output
        cleanup                cleans up cached downloads and tracker symlinks
        create                 creates the given Cask and opens it in an editor
        doctor                 checks for configuration issues
        edit                   edits the given Cask
        fetch                  downloads remote application files to local cache
        home                   opens the homepage of the given Cask
        info                   displays information about the given Cask
        install                installs the given Cask
        list                   with no args, lists installed Casks; given installed Casks, lists staged files
        search                 searches all known Casks
        style                  checks Cask style using RuboCop
        uninstall              uninstalls the given Cask
        update                 a synonym for 'brew update'
        zap                    zaps all files associated with the given Cask
    
    See also "man brew-cask"

--- brew-full-upgrade.sh
+++ brew-full-upgrade.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+##### COLORS
+BLUE(){  tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
+WHITE(){ tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
+##### END COLORS
+
 brew cleanup; brew cask cleanup
 brew update
 brew upgrade
 for pkg in $(brew cask list); do
     pkg_latest=$(brew cask info $pkg | grep $pkg: | awk '{print $2}')
     [ "$pkg_latest" == "latest" ] && continue
-    if [ ! -d /usr/local/Caskroom/$pkg/$pkg_latest ]; then
-        echo $pkg
-        brew cask fetch $pkg
-        brew cask install $pkg --force
-        echo
-    fi
+    [ ! -d /usr/local/Caskroom/$pkg/$pkg_latest ] && pkgs="$pkgs $pkg"
+done
+[ -n "$pkgs" ] || exit 0
+
+BLUE "==> "; WHITE "Upgrading $(echo $pkgs | wc -w) outdated Casks, with result:\\n"
+echo $pkgs
+for pkg in $pkgs; do
+    brew cask fetch $pkg
+    brew cask install $pkg --force
 done
