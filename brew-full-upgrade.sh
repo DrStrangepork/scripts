f9b6ef26c05a3deb6948c315cc7345125de63d12
DrStrangepork <rickkasten@gmail.com>
2017-07-13 12:00:25 -0400

    Cosmetics

--- brew-full-upgrade.sh
+++ brew-full-upgrade.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ##### COLORS
-BLUE(){  tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
-WHITE(){ tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
+BLUE()  { tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
+WHITE() { tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
 ##### END COLORS
 
 brew cleanup; brew cask cleanup
@@ -13,7 +13,7 @@ for pkg in $(brew cask list); do
     [ "$pkg_latest" == "latest" ] && continue
     [ ! -d /usr/local/Caskroom/$pkg/$pkg_latest ] && pkgs="$pkgs $pkg"
 done
-[ -n "$pkgs" ] || exit 0
+[ -n "$pkgs" ] && echo || exit 0
 
 BLUE "==> "; WHITE "Upgrading $(echo $pkgs | wc -w) outdated Casks, with result:\\n"
 echo $pkgs
