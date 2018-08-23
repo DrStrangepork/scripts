bbf349336c7e2ea3f6c5778bed2cd63c50dfcc72
DrStrangepork <rickkasten@gmail.com>
2018-08-22 20:04:10 -0400

    brew cask cleanup deprecated

--- brew-cask-upgrade.sh
+++ brew-cask-upgrade.sh
@@ -5,7 +5,6 @@ BLUE()  { tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
 WHITE() { tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
 ##### END COLORS
 
-brew cask cleanup
 packages="$(brew cask outdated --quiet)"
 [ -n "$packages" ] && echo || exit 0
 
