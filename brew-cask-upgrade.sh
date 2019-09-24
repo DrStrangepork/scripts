e40ae3f0e14d3cf95c465fb56658cbb4b801d1e0
DrStrangepork <rickkasten@gmail.com>
2019-09-24 03:43:54 -0400

    cask-retire.sh

--- brew-cask-upgrade.sh
+++ brew-cask-upgrade.sh
@@ -5,7 +5,7 @@ BLUE()  { tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
 WHITE() { tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
 ##### END COLORS
 
-packages="$(brew cask outdated --quiet)"
+packages="$(brew cask outdated --greedy --quiet)"
 [ -n "$packages" ] && echo || exit 0
 
 BLUE "==> "; WHITE "Upgrading $(echo $packages | wc -w) outdated Casks, with result:\\n"
@@ -13,5 +13,6 @@ echo $packages
 for pkg in $packages; do
     brew cask fetch $pkg
     brew cask install $pkg --force
+    cask-retire.sh
     echo
 done
