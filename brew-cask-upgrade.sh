6af7db4d716211e99f2a7cf516ef6ef635fca3d9
DrStrangepork <rickkasten@gmail.com>
2019-09-26 07:23:26 -0400

    Removing greedy as default

--- brew-cask-upgrade.sh
+++ brew-cask-upgrade.sh
@@ -5,7 +5,8 @@ BLUE()  { tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
 WHITE() { tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
 ##### END COLORS
 
-packages="$(brew cask outdated --greedy --quiet)"
+# packages="$(brew cask outdated --greedy --quiet)"
+packages="$(brew cask outdated --quiet)"
 [ -n "$packages" ] && echo || exit 0
 
 BLUE "==> "; WHITE "Upgrading $(echo $packages | wc -w) outdated Casks, with result:\\n"
