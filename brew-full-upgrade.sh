3b5a3b3e9438f41cdf5e1a9ff8f7dd13ae3685ce
DrStrangepork <rickkasten@gmail.com>
2020-03-12 21:26:30 -0400

    simpler brew-full-upgrade.sh

--- brew-full-upgrade.sh
+++ brew-full-upgrade.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+##### COLORS
+BLUE()  { tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
+WHITE() { tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
+##### END COLORS
+
 brew cleanup
 brew update
 brew upgrade
-brew-cask-upgrade.sh
+
+packages="$(brew cask outdated --greedy --verbose | awk '$NF !~ /latest/ {print $1}')"
+[ -n "$packages" ] && brew cask reinstall $packages
