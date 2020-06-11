4dd2cedd9b55e1b9882a99db02270a7295d60df6
Rick Kasten <rick.kasten@libertymutual.com>
2020-06-11 11:02:16 -0400

    brew-full-upgrade --greedy

--- brew-full-upgrade.sh
+++ brew-full-upgrade.sh
@@ -5,13 +5,38 @@ BLUE()  { tput bold; tput setaf 4; echo -e -n "$@"; tput sgr0; }
 WHITE() { tput bold; tput setaf 7; echo -e -n "$@"; tput sgr0; }
 ##### END COLORS
 
+
+usage() {
+  echo \
+"$(tput bold)$(basename ${BASH_SOURCE[0]})$(tput sgr0)
+Usage:  Upgrade all brew packages and casks
+Options:
+  -g            \`brew cask upgrade --greedy\`
+  -h            help"
+}
+
+
+[[ "$*" =~ "--help" ]] && { usage | less; exit; }
+while getopts ":gh" opt; do
+  case $opt in
+    g)  GREEDY=--greedy
+        ;;
+    h)  usage ; exit
+        ;;
+    *)  echo "Error: invalid option -$OPTARG" >&2
+        usage ; exit 1
+        ;;
+  esac
+done
+
+
+## MAIN
 brew cleanup
 brew update
 brew upgrade
 
-packages="$(brew cask outdated --greedy --verbose | awk '$NF !~ /latest/ {print $1}')"
-[ -n "$packages" ] && echo || exit 0
-
-BLUE "==> "; WHITE "Upgrading $(echo $packages | wc -w) outdated Casks, with result:\\n"
+packages="$(brew cask outdated $GREEDY)"
+[ -z "$packages" ] && { echo "No casks to upgrade"; exit 0; }
+BLUE "\n==> "; WHITE "Upgrading $(echo $packages | wc -w) outdated Casks, with result:\\n"
 echo $packages
-brew cask reinstall $packages
+brew cask upgrade $GREEDY
