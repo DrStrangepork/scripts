7d9cbac3fac76cd7068480993180f5486185252a
Rick Kasten <rick.kasten@libertymutual.com>
2022-09-07 11:21:17 -0400

    jdumps()

--- pdbstart.py
+++ pdbstart.py
@@ -3,4 +3,12 @@ import json
 import boto3
 from botocore.exceptions import ClientError, WaiterError
 import pdb
+
+
+def jdumps(dict):
+    print(json.JSONEncoder(default=str, sort_keys=True).encode(dict))
+
+
+ec2 = boto3.client('ec2')
+
 pdb.set_trace()
