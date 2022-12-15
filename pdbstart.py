c808b8cc39b21122fefae4834419167757e60197
Rick Kasten <rick.kasten@libertymutual.com>
2022-12-15 14:44:38 -0500

    latest pdbstart

--- pdbstart.py
+++ pdbstart.py
@@ -2,15 +2,36 @@
 import datetime
 import json
 import pdb
+import pprint
 import boto3
-from botocore.exceptions import ClientError, WaiterError
+import botocore.exceptions
+
+
+def awsacct():
+    return boto3.client('sts').get_caller_identity().get('Account')
+
+
+def awsalias():
+    return boto3.client('iam').list_account_aliases()['AccountAliases'][0]
 
 
 def jdumps(dict):
     print(json.JSONEncoder(default=str, sort_keys=True).encode(dict))
 
 
-ec2 = boto3.client('ec2')
+def pp(dict):
+    pprint.PrettyPrinter(indent=2).pprint(dict)
+
+
+# for key, value in sorted(botocore.exceptions.__dict__.items()):
+#     if isinstance(value, type):
+#         print(key)
+
+try:
+    ec2 = boto3.client('ec2')
+except botocore.exceptions.NoRegionError:
+    print('No active AWS profile')
+    pass
 now = datetime.datetime.now()
 
 pdb.set_trace()
